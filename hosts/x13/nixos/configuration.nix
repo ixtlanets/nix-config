87a834db643f3b5d115e87964ee6133e27969fa1
Sergey Nikulin <snikulin@gmail.com>
2023-06-06 16:59:03 +0300

    correct host config

--- hosts/x13/nixos/configuration.nix
+++ hosts/x13/nixos/configuration.nix
@@ -15,9 +15,8 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "x1carbon"; # Define your hostname.
+  networking.hostName = "x13"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
