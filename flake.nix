92ce741c5c7aa5dbed14da4f20127ca770a362c9
Sergey Nikulin <snikulin@gmail.com>
2023-07-22 14:27:30 +0300

    rec was unused

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
         "x86_64-darwin"
       ];
     in
-    rec {
+    {
       # Your custom packages
       # Acessible through 'nix build', 'nix shell', etc
       packages = forAllSystems (system:
