66ea76192e77dbce5f0868190bf3fa04f577248b
Sergey Nikulin <snikulin@gmail.com>
2023-07-23 00:30:23 +0300

    switch to grub

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -2,8 +2,11 @@
 
 {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 8;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.configurationLimit = 8;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
