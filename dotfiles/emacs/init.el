4fec7be1cb4f81d219a81a58c9145a771c21809f
Sergey Nikulin <snikulin@gmail.com>
2023-07-29 23:42:30 +0300

    measure startup time

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -347,3 +347,15 @@
       (org-babel-tangle))))
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
+
+;; Startup time
+(defun efs/display-startup-time ()
+  (message
+   "Emacs loaded in %s with %d garbage collections."
+   (format
+    "%.2f seconds"
+    (float-time
+     (time-subtract after-init-time before-init-time)))
+   gcs-done))
+
+(add-hook 'emacs-startup-hook #'efs/display-startup-time)
