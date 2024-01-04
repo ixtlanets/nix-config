305089f3ac3ec0ef29c2c5d8d0ff6f676ff5750e
Sergey Nikulin <snikulin@gmail.com>
2024-01-05 00:37:50 +0300

    wayland is buggy with nvidia drivers

--- hosts/zenbook/home-manager/home.nix
+++ hosts/zenbook/home-manager/home.nix
@@ -18,8 +18,7 @@
     ../../../modules/home-manager/emacs.nix
     ../../../modules/home-manager/linux-desktop.nix
     ../../../modules/home-manager/gnome.nix
-    ../../../modules/home-manager/sway.nix
-    ../../../modules/home-manager/kanshi.nix
+    ../../../modules/home-manager/i3.nix
     ../../../modules/home-manager/email.nix
     ../../../modules/home-manager/nvim.nix
   ];
