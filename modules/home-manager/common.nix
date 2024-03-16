c697c4235627d08e911a8a828915d349a676a5a3
Sergey Nikulin <snikulin@gmail.com>
2024-03-16 23:48:43 +0300

    zsh options changed

--- modules/home-manager/common.nix
+++ modules/home-manager/common.nix
@@ -119,7 +119,7 @@ in {
     };
     zsh = {
       enable = true;
-      enableAutosuggestions = true;
+      autosuggestion.enable = true;
       enableCompletion = true;
       syntaxHighlighting.enable = true;
       history = {
