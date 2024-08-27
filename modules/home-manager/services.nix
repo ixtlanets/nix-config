00c48829741bd2371c8417e6ba8d3d429834bf08
Sergey Nikulin <snikulin@gmail.com>
2024-08-27 13:18:37 +0300

    re-enable syncthing :)

--- modules/home-manager/services.nix
+++ modules/home-manager/services.nix
@@ -1,6 +1,6 @@
 { inputs, outputs, lib, config, pkgs, ... }: 
 {
-
+  services.syncthing.enable = true;
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 }
