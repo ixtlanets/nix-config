8713bce3be1cd1ab1830ae4ff6acfcfd36a1ad92
Sergey Nikulin <snikulin@gmail.com>
2024-12-05 14:25:54 +0300

    hypridle

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -166,6 +166,28 @@
   home.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
   home.sessionVariables.ELECTRON_OZONE_PLATFORM_HINT = "auto";
   services.network-manager-applet.enable = true;
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = "hyprlock";
+      };
+
+      listener = [
+        {
+          timeout = 900;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 1200;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+    };
+  };
   home.file.".config/electron-flags.conf".text = ''
 --enable-features=WaylandWindowDecorations
 --ozone-platform-hint=auto
