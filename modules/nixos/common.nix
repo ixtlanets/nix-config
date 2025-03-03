4e1a03dbb63f63498557f99460cd38e22ee6e67a
Sergey Nikulin <snikulin@gmail.com>
2025-03-03 12:30:08 +0300

    revert back to ordinary docker

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -197,15 +197,7 @@ in
       enableKvm = true;
       addNetworkInterface = false;
     };
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
+    docker.enable = true;
   };
 
   services.power-profiles-daemon.enable = true;
