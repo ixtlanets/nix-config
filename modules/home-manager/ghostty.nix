51281a4272bdc117db551acea0369873e466da36
Sergey Nikulin <snikulin@gmail.com>
2025-03-18 21:31:10 +0300

    better ghostty config

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -7,12 +7,12 @@
   ...
 }:
 {
-  home.file.".config/ghostty/config".text = ''
-    font-family = "Hack Nerd Font"
-    font-size = 16
-    theme = catppuccin-mocha
-    quit-after-last-window-closed = true
-    macos-non-native-fullscreen = true
-    macos-option-as-alt = true
-  '';
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "catppuccin-mocha";
+      font-family = "Hack Nerd Font";
+      font-size = 16;
+    };
+  };
 }
