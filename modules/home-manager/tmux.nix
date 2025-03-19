3bc4d58fac229aad4cf64b17f057d7e7114022d6
Sergey Nikulin <snikulin@gmail.com>
2025-03-19 12:00:43 +0300

    remove weather info
    to save some space and get rid of bug when 'rev' command eatch cpu

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,18 +1,25 @@
-{ inputs, outputs, lib, config, pkgs, ... }: 
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   nixpkgs = {
     overlays = [
       (final: prev: {
-       tmuxPluginsDracula = final.tmuxPlugins.dracula.overrideAttrs (oldAttrs: {
-           version = "2.2.0";
-           src = pkgs.fetchFromGitHub {
-           owner = "dracula";
-           repo = "tmux";
-           rev = "v2.2.0";
-           sha256 = "9p+KO3/SrASHGtEk8ioW+BnC4cXndYx4FL0T70lKU2w=";
-           };
-           });
-       })
+        tmuxPluginsDracula = final.tmuxPlugins.dracula.overrideAttrs (oldAttrs: {
+          version = "2.2.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "dracula";
+            repo = "tmux";
+            rev = "v2.2.0";
+            sha256 = "9p+KO3/SrASHGtEk8ioW+BnC4cXndYx4FL0T70lKU2w=";
+          };
+        });
+      })
     ];
   };
   programs.tmux = {
@@ -22,27 +29,27 @@
     keyMode = "vi";
     plugins = [
       pkgs.tmuxPlugins.sensible
-        pkgs.tmuxPlugins.pain-control
-        pkgs.tmuxPlugins.urlview
-        pkgs.tmuxPlugins.prefix-highlight
-        {
-          plugin = pkgs.tmuxPluginsDracula;
-          extraConfig = ''
-            set -g @dracula-show-fahrenheit false
-            set -g @dracula-plugins "battery cpu-usage ram-usage weather time"
-            set -g @dracula-show-left-icon session
-            set -g @dracula-day-month true
-            set -g @dracula-military-time true
+      pkgs.tmuxPlugins.pain-control
+      pkgs.tmuxPlugins.urlview
+      pkgs.tmuxPlugins.prefix-highlight
+      {
+        plugin = pkgs.tmuxPluginsDracula;
+        extraConfig = ''
+          set -g @dracula-show-fahrenheit false
+          set -g @dracula-plugins "battery cpu-usage ram-usage time"
+          set -g @dracula-show-left-icon session
+          set -g @dracula-day-month true
+          set -g @dracula-military-time true
 
-            set -g allow-passthrough on
+          set -g allow-passthrough on
 
-            set -ga update-environment TERM
-            set -ga update-environment TERM_PROGRAM
-            '';
-        }
+          set -ga update-environment TERM
+          set -ga update-environment TERM_PROGRAM
+        '';
+      }
     ];
     extraConfig = ''
 
-      '';
+    '';
   };
 }
