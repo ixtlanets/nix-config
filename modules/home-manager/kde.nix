047fcd63ac1f819aac62f589d3bcf07282b9a422
Sergey Nikulin <snikulin@gmail.com>
2025-03-26 02:42:13 +0300

    Move windows shortcuts

--- modules/home-manager/kde.nix
+++ modules/home-manager/kde.nix
@@ -211,6 +211,11 @@
         "Switch to Desktop 3" = "Meta+3";
         "Switch to Desktop 4" = "Meta+4";
         "Switch to Desktop 5" = "Meta+5";
+        "Window to Desktop 1" = "Meta+!";
+        "Window to Desktop 2" = "Meta+@";
+        "Window to Desktop 3" = "Meta+#";
+        "Window to Desktop 4" = "Meta+$";
+        "Window to Desktop 5" = "Meta+%";
         "Switch One Desktop to the Left" = "Meta+Ctrl+Left";
         "Switch One Desktop to the Right" = "Meta+Ctrl+Right";
       };
