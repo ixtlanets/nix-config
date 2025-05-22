103c840df4048ba07b2f9efd2d66cad753acad2a
Sergey Nikulin <snikulin@gmail.com>
2025-05-22 21:38:15 +0300

    add swap
    sometimes I'm getting OOM

--- hosts/x13/nixos/hardware-configuration.nix
+++ hosts/x13/nixos/hardware-configuration.nix
@@ -13,7 +13,10 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 16*1024; # 16 GB
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
