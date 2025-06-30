fa43fd5c779436c4a22048d82ee7f85ea58aa73c
Sergey Nikulin <snikulin@gmail.com>
2025-06-30 14:20:03 +0300

    proper switch to Brave for mac

--- hosts/m1max/home-manager/home.nix
+++ hosts/m1max/home-manager/home.nix
@@ -121,9 +121,8 @@ in
     macos-option-as-alt = true
   '';
   programs = {
-    chromium = {
+    brabe = {
       enable = true;
-      package = pkgs.brave;
       extensions = [
         { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
         { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1Password
