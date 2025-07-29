08e3c0d2e831b090f710af4a6d2d657ac23470b0
Sergey Nikulin <snikulin@gmail.com>
2025-07-29 14:57:41 +0300

    gnome

--- hosts/um960pro/nixos/configuration.nix
+++ hosts/um960pro/nixos/configuration.nix
@@ -19,8 +19,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../../modules/nixos/common.nix
-    ../../../modules/nixos/hyprland.nix
-    ../../../modules/nixos/nautilus.nix
+    ../../../modules/nixos/gnome.nix
   ];
 
   boot.loader.efi.efiSysMountPoint = lib.mkForce "/boot";
