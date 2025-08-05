06f3da4f69757db7896f5c775d60de3b2b06c5a6
Sergey Nikulin <snikulin@gmail.com>
2025-08-05 21:37:47 +0300

    flake update

--- modules/home-manager/linux-desktop.nix
+++ modules/home-manager/linux-desktop.nix
@@ -239,13 +239,6 @@ in
   };
 
   gtk.enable = true;
-  gtk.iconTheme = {
-    name = "Papirus-Dark";
-    package = pkgs.catppuccin-papirus-folders.override {
-      flavor = "mocha";
-      accent = "maroon";
-    };
-  };
   qt.enable = true;
   qt.style.name = "kvantum";
   qt.platformTheme.name = "kvantum";
