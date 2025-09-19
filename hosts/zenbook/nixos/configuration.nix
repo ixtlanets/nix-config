90555f069ba42719e5d6ed1ec4f8a3a6e23a8bcc
Sergey Nikulin <snikulin@gmail.com>
2025-09-20 01:32:50 +0300

    back to hyprland

--- hosts/zenbook/nixos/configuration.nix
+++ hosts/zenbook/nixos/configuration.nix
@@ -18,7 +18,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../../modules/nixos/common.nix
-    ../../../modules/nixos/kde.nix
+    ../../../modules/nixos/hyprland.nix
   ];
 
   boot.loader.efi.efiSysMountPoint = lib.mkForce "/boot";
