8a6be5aae190bf55bb9f881504af599bcd6dfaac
Sergey Nikulin <snikulin@gmail.com>
2025-09-21 01:57:39 +0300

    gnome: add hypr-like keybindings

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -5,6 +5,33 @@
 }:
 let
   mkTuple = lib.hm.gvariant.mkTuple;
+  workspaceNumbers = lib.range 1 9;
+  workspaceKeybindings =
+    lib.foldl'
+      (acc: num:
+        let
+          key = builtins.toString num;
+        in
+        acc
+        // {
+          "switch-to-workspace-${key}" = [
+            "<Super>${key}"
+            "<Ctrl><Alt>${key}"
+          ];
+          "move-to-workspace-${key}" = [
+            "<Super><Shift>${key}"
+            "<Shift><Ctrl><Alt>${key}"
+          ];
+        }
+      )
+      {}
+      workspaceNumbers;
+  appSwitcherDisable = lib.listToAttrs (
+    map (num: {
+      name = "switch-to-application-${builtins.toString num}";
+      value = [ ];
+    }) workspaceNumbers
+  );
 in
 {
   home.sessionVariables = {
@@ -17,6 +44,7 @@ in
     gnomeExtensions.bluetooth-quick-connect
     gnomeExtensions.quick-lang-switch
     gnomeExtensions.dash-to-panel
+    gnomeExtensions.just-perfection
   ];
   dconf.settings = {
     "org/gnome/desktop/interface" = {
@@ -46,30 +74,23 @@ in
         "grp:win_space_toggle"
       ];
     };
-    "org/gnome/desktop/wm/keybindings" = {
-      close = [
-        "<Super>q"
-        "<Alt>F4"
-      ];
-      maximize = [ "<Super>Up" ];
-      unmaximize = [ "<Super>Down" ];
-      toggle-fullscreen = [ "<Super>f" ];
-      switch-to-workspace-left = [ "<Ctrl><Alt>Left" ];
-      switch-to-workspace-right = [ "<Ctrl><Alt>Right" ];
-      switch-to-workspace-1 = [ "<Ctrl><Alt>1" ];
-      switch-to-workspace-2 = [ "<Ctrl><Alt>2" ];
-      switch-to-workspace-3 = [ "<Ctrl><Alt>3" ];
-      switch-to-workspace-4 = [ "<Ctrl><Alt>4" ];
-      switch-to-workspace-5 = [ "<Ctrl><Alt>5" ];
-      move-to-workspace-left = [ "<Shift><Ctrl><Alt>Left" ];
-      move-to-workspace-right = [ "<Shift><Ctrl><Alt>Right" ];
-      move-to-workspace-1 = [ "<Shift><Ctrl><Alt>1" ];
-      move-to-workspace-2 = [ "<Shift><Ctrl><Alt>2" ];
-      move-to-workspace-3 = [ "<Shift><Ctrl><Alt>3" ];
-      move-to-workspace-4 = [ "<Shift><Ctrl><Alt>4" ];
-      move-to-workspace-5 = [ "<Shift><Ctrl><Alt>5" ];
-      activate-window-menu = [ ]; # Disable Alt+Space for window menu
-    };
+    "org/gnome/desktop/wm/keybindings" =
+      workspaceKeybindings
+      // {
+        close = [
+          "<Super>w"
+          "<Alt>F4"
+        ];
+        maximize = [ "<Super>Up" ];
+        unmaximize = [ "<Super>Down" ];
+        toggle-fullscreen = [ "<Super>f" ];
+        switch-to-workspace-left = [ "<Ctrl><Alt>Left" ];
+        switch-to-workspace-right = [ "<Ctrl><Alt>Right" ];
+        move-to-workspace-left = [ "<Shift><Ctrl><Alt>Left" ];
+        move-to-workspace-right = [ "<Shift><Ctrl><Alt>Right" ];
+        activate-window-menu = [ ]; # Disable Alt+Space for window menu
+      };
+    "org/gnome/shell/keybindings" = appSwitcherDisable;
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
@@ -98,6 +119,25 @@ in
       command = "kbd-backlight toggle";
       name = "toggle-keyboard-backlight";
     };
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = lib.hm.gvariant.mkUint32 9;
+      workspace-names = lib.hm.gvariant.mkArray
+        lib.hm.gvariant.type.string
+        [
+          "1"
+          "2"
+          "3"
+          "4"
+          "5"
+          "6"
+          "7"
+          "8"
+          "9"
+        ];
+    };
     "org/gnome/shell" = {
       favorite-apps = [
         "brave-browser.desktop"
@@ -116,6 +156,9 @@ in
         "quick-lang-switch@ankostis.gmail.com"
         "dash-to-panel@jderose9.github.com"
       ];
+    };
+    "org/gnome/shell/extensions/just-perfection" = {
+      workspace-animation = false;
     };
     "org/gnome/clocks" = {
       world-clocks = "[{'location': <(uint32 2, <('San Francisco', 'KOAK', true, [(0.65832848982162007, -2.133408063190589)], [(0.659296885757089, -2.1366218601153339)])>)>}, {'location': <(uint32 2, <('New York', 'KNYC', true, [(0.71180344078725644, -1.2909618758762367)], [(0.71059804659265924, -1.2916478949920254)])>)>}, {'location': <(uint32 2, <('London', 'EGWU', true, [(0.89971722940307675, -0.007272211034407213)], [(0.89884456477707964, -0.0020362232784242244)])>)>}]";
