62fcfc78929b46ac9c0988fcfed701f750863319
Sergey Nikulin <snikulin@gmail.com>
2025-09-28 20:30:51 +0300

    pkgs: specify-cli pin truststore 0.10.4

--- pkgs/specify-cli.nix
+++ pkgs/specify-cli.nix
@@ -3,6 +3,17 @@
 , fetchFromGitHub
 }:
 
+let
+  truststore_0_10_4 = python3Packages.truststore.overridePythonAttrs (_: rec {
+    version = "0.10.4";
+    src = python3Packages.fetchPypi {
+      pname = "truststore";
+      inherit version;
+      sha256 = "sha256-nZG9Q2RjrV5O5KunZmKN1s1wEM8+JGF1azMDcQ7rwwE=";
+    };
+  });
+in
+
 python3Packages.buildPythonApplication rec {
   pname = "specify-cli";
   version = "0.0.54";
@@ -19,16 +30,17 @@ python3Packages.buildPythonApplication rec {
     hatchling
   ];
 
-  propagatedBuildInputs = with python3Packages; [
-    typer
-    rich
-    httpx
-    platformdirs
-    readchar
-    truststore
-  ] ++ [
-    httpx-socks
-  ];
+  propagatedBuildInputs =
+    (with python3Packages; [
+      typer
+      rich
+      httpx
+      platformdirs
+      readchar
+      httpx-socks
+    ]) ++ [
+      truststore_0_10_4
+    ];
 
   meta = {
     description = "Specify CLI for Spec-Driven Development";
