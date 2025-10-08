78eea0024bd08c28304e98c02cf94d4574555c4b
Sergey Nikulin <snikulin@gmail.com>
2025-10-08 03:00:59 +0300

    format: nixpkgs-fmt overlays and pkgs

--- pkgs/opencode.nix
+++ pkgs/opencode.nix
@@ -1,7 +1,7 @@
-{
-  lib,
-  stdenv,
-  fetchzip,
+{ lib
+, stdenv
+, fetchzip
+,
 }:
 stdenv.mkDerivation rec {
   pname = "opencode";
@@ -9,29 +9,33 @@ stdenv.mkDerivation rec {
 
   src =
     if stdenv.isLinux && stdenv.isx86_64 then
-      fetchzip {
-        url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-x64.zip";
-        sha256 = "sha256-SLQt+3AwwSwTdoDTwEAKQov+R1Xlx+pJIy6eA3rWEtQ=";
-        stripRoot = false;
-      }
+      fetchzip
+        {
+          url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-x64.zip";
+          sha256 = "sha256-SLQt+3AwwSwTdoDTwEAKQov+R1Xlx+pJIy6eA3rWEtQ=";
+          stripRoot = false;
+        }
     else if stdenv.isLinux && stdenv.isAarch64 then
-      fetchzip {
-        url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-arm64.zip";
-        sha256 = "sha256-yeJCB6sIcfjyLbdHdpE6N+S2HZXfKI+nC99nXApXQvw=";
-        stripRoot = false;
-      }
+      fetchzip
+        {
+          url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-arm64.zip";
+          sha256 = "sha256-yeJCB6sIcfjyLbdHdpE6N+S2HZXfKI+nC99nXApXQvw=";
+          stripRoot = false;
+        }
     else if stdenv.isDarwin && stdenv.isx86_64 then
-      fetchzip {
-        url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-darwin-x64.zip";
-        sha256 = "sha256-dBlVCQWlungJ2aNXP8dKN3EDSjNIRp/yVTNiqgQFqxs=";
-        stripRoot = false;
-      }
+      fetchzip
+        {
+          url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-darwin-x64.zip";
+          sha256 = "sha256-dBlVCQWlungJ2aNXP8dKN3EDSjNIRp/yVTNiqgQFqxs=";
+          stripRoot = false;
+        }
     else if stdenv.isDarwin && stdenv.isAarch64 then
-      fetchzip {
-        url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-darwin-arm64.zip";
-        sha256 = "sha256-3LGH1TnzhhSQ+tWNtfTue1vXfTgaCBsKx0Ij3MSEP3g=";
-        stripRoot = false;
-      }
+      fetchzip
+        {
+          url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-darwin-arm64.zip";
+          sha256 = "sha256-3LGH1TnzhhSQ+tWNtfTue1vXfTgaCBsKx0Ij3MSEP3g=";
+          stripRoot = false;
+        }
     else
       throw "Unsupported system for opencode";
 
