12190082f3103a294e5c29dc79303795ef71197b
Sergey Nikulin <snikulin@gmail.com>
2025-11-04 16:06:33 +0300

    opencode versions bump

--- pkgs/opencode.nix
+++ pkgs/opencode.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "opencode";
-  version = "1.0.17";
+  version = "1.0.20";
 
   src =
     if stdenv.isLinux && stdenv.isx86_64 then
       fetchzip {
         url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-linux-x64.zip";
-        sha256 = "sha256-IcJiCZb8Tl2PGx+qU/Hx568axasVA2VJHdma/Z0puDQ=";
+        sha256 = "sha256-0ZMpdScnMzy6obd7jpD1WP83jrfJRC1cUob4mLbdxpE=";
         stripRoot = false;
       }
     else if stdenv.isLinux && stdenv.isAarch64 then
