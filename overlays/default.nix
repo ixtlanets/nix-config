b3960489494c0a487a3b70cab2ac263fce1ec2ce
Sergey Nikulin <snikulin@gmail.com>
2025-11-07 21:55:49 +0300

    codex version bump

--- overlays/default.nix
+++ overlays/default.nix
@@ -18,7 +18,7 @@
   modifications = final: prev: {
     tawm = inputs.tawm.packages.${prev.system}.default;
     codex = prev.codex.overrideAttrs (old: rec {
-      version = "0.55.0";
+      version = "0.56.0";
       buildType = "simple";
       cargoSetupPostPatchHook = ":";
       nativeBuildInputs = [
@@ -29,7 +29,7 @@
         if prev.stdenv.isLinux && prev.stdenv.isx86_64 then
           prev.fetchurl {
             url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-x86_64-unknown-linux-musl.zst";
-            sha256 = "sha256-SD3R1MV+AtnGWIKX/vWvetWPTE4/wco7E0OZaUCuOjk=";
+            sha256 = "sha256-GiQsa0UN5XbBRSe8ED3HfHo8VBqvUdj5tEdCZE3pTWQ=";
           }
         else if prev.stdenv.isLinux && prev.stdenv.isAarch64 then
           prev.fetchurl {
