1184f4030476ccbb93f4b14f339b8b3dcb42f374
Sergey Nikulin <snikulin@gmail.com>
2025-12-21 00:46:26 +0300

    add nik to nix trusted users

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   nixConfig = {
     extra-trusted-substituters = [ "https://cache.flox.dev" ];
     extra-trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
+    extra-trusted-users = [ "nik" ];
   };
 
   inputs = {
