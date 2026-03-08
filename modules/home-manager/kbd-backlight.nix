341e0cd696aa758816d718480124aff832b7c0a4
Sergey Nikulin <snikulin@gmail.com>
2026-03-08 11:09:24 +0300

    home-manager: fix screenshot and keyboard backlight helpers

--- modules/home-manager/kbd-backlight.nix
+++ modules/home-manager/kbd-backlight.nix
@@ -4,30 +4,48 @@
     home.packages = [
       (pkgs.writeShellScriptBin "kbd-backlight" ''
         #!/bin/sh
+        set -eu
 
-        # Find the keyboard backlight device
-        for device in $(light -L | grep "kbd_backlight" | sed 's/.*sysfs\/leds\///')
-        do
-          KBD_DEVICE="sysfs/leds/$device"
-          break
-        done
+        brightnessctl_bin="${pkgs.brightnessctl}/bin/brightnessctl"
+        awk_bin="${pkgs.gawk}/bin/awk"
 
-        if [ -z "$KBD_DEVICE" ]; then
+        kbd_device="$($brightnessctl_bin -l | $awk_bin -F"'" '/::kbd_backlight/ { print $2; exit }')"
+
+        if [ -z "$kbd_device" ]; then
           echo "No keyboard backlight device found"
           exit 1
         fi
 
-        if [ "$1" = "up" ]; then
-          light -Ars "$KBD_DEVICE" 50
-        elif [ "$1" = "down" ]; then
-          light -Urs "$KBD_DEVICE" 50
-        elif [ "$1" = "toggle" ]; then
-          if [ "$(light -rs "$KBD_DEVICE")" = "0" ]; then
-            light -Srs "$KBD_DEVICE" 100
-          else
-            light -Srs "$KBD_DEVICE" 0
-          fi
-        fi
+        current="$($brightnessctl_bin -d "$kbd_device" g)"
+        max="$($brightnessctl_bin -d "$kbd_device" m)"
+
+        case "''${1:-toggle}" in
+          up)
+            next=$((current + 1))
+            if [ "$next" -gt "$max" ]; then
+              next="$max"
+            fi
+            $brightnessctl_bin -d "$kbd_device" s "$next" >/dev/null
+            ;;
+          down)
+            next=$((current - 1))
+            if [ "$next" -lt 0 ]; then
+              next=0
+            fi
+            $brightnessctl_bin -d "$kbd_device" s "$next" >/dev/null
+            ;;
+          toggle)
+            if [ "$current" -eq 0 ]; then
+              $brightnessctl_bin -d "$kbd_device" s "$max" >/dev/null
+            else
+              $brightnessctl_bin -d "$kbd_device" s 0 >/dev/null
+            fi
+            ;;
+          *)
+            echo "Usage: kbd-backlight [up|down|toggle]"
+            exit 2
+            ;;
+        esac
       '')
     ];
   };
