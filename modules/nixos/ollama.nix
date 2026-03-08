ccb0ec7f4b1ec514af19f9a1ec3153ceaab63e04
Sergey Nikulin <snikulin@gmail.com>
2026-03-09 00:51:54 +0300

    ollama: increase default context length

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -1,10 +1,13 @@
 { lib, config, ... }:
 let
   defaultOriginAllowList = "chrome-extension://*";
+  defaultContextLength = "65536";
 in
 {
   config = lib.mkIf config.services.ollama.enable {
-    systemd.services.ollama.environment.OLLAMA_ORIGINS =
-      lib.mkDefault defaultOriginAllowList;
+    systemd.services.ollama.environment = {
+      OLLAMA_CONTEXT_LENGTH = lib.mkDefault defaultContextLength;
+      OLLAMA_ORIGINS = lib.mkDefault defaultOriginAllowList;
+    };
   };
 }
