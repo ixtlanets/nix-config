6cfb76d369a75c097eca5263f5dc3cde4e10ce17
Sergey Nikulin <snikulin@gmail.com>
2026-04-17 00:56:57 +0300

    home-manager: track m1max syncthing identity

--- hosts/m1max/home-manager/syncthing.nix
+++ hosts/m1max/home-manager/syncthing.nix
@@ -1,6 +1,6 @@
 import ../../../modules/home-manager/syncthing-topology.nix {
-  cert = "/Users/nik/Library/Application Support/Syncthing/cert.pem";
-  key = "/Users/nik/Library/Application Support/Syncthing/key.pem";
+  cert = "/Users/nik/nix-config/secrets/syncthing/m1max/cert.pem";
+  key = "/Users/nik/nix-config/secrets/syncthing/m1max/key.pem";
   syncthingHome = "/Users/nik/Library/Application Support/Syncthing";
   folderPaths = {
     "3y3qt-shfv6" = "/Users/nik/obsidian-vault";
