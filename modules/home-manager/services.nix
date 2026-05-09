4ac89ab56238ea26c077377725f29beb3d09846f
Sergey Nikulin <snikulin@gmail.com>
2026-05-09 21:45:29 +0300

    home-manager: mount goar-lbrand Drive

--- modules/home-manager/services.nix
+++ modules/home-manager/services.nix
@@ -1,15 +1,49 @@
 {
-  inputs,
-  outputs,
   lib,
   config,
   pkgs,
   ...
 }:
+let
+  mountPoint = "${config.home.homeDirectory}/goar-lbrand";
+  rcloneConfig = "${config.xdg.configHome}/rclone/rclone.conf";
+  rcloneConfigSecret = "${config.home.homeDirectory}/nix-config/secrets/rclone/rclone.conf";
+in
 {
+  home.packages = lib.optionals pkgs.stdenv.isLinux [ pkgs.rclone ];
+
+  xdg.configFile."rclone/rclone.conf" = lib.mkIf pkgs.stdenv.isLinux {
+    source = config.lib.file.mkOutOfStoreSymlink rcloneConfigSecret;
+    force = true;
+  };
+
   services.syncthing.enable = true;
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
+
+  systemd.user.services.rclone-goar-lbrand = lib.mkIf pkgs.stdenv.isLinux {
+    Unit = {
+      Description = "Mount goar-lbrand Google Drive";
+      ConditionPathExists = rcloneConfig;
+    };
+
+    Service = {
+      Type = "simple";
+      Environment = "PATH=/run/wrappers/bin:${pkgs.fuse3}/bin";
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPoint}";
+      ExecStart = ''
+        ${pkgs.rclone}/bin/rclone mount goar-lbrand: ${mountPoint} \
+          --config ${rcloneConfig} \
+          --vfs-cache-mode writes \
+          --log-level INFO
+      '';
+      ExecStop = "/run/wrappers/bin/fusermount3 -u ${mountPoint}";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+
+    Install.WantedBy = [ "default.target" ];
+  };
 
   systemd.user.services.voxtype = {
     Unit = {
