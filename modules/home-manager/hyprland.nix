257fc24b49d2ea88e0b2c00d3e557f885fc90314
Sergey Nikulin <snikulin@gmail.com>
2026-05-27 19:23:41 +0300

    hyprland: fix catppuccin after update

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -1,7 +1,8 @@
-{ pkgs
-, lib
-, dpi
-, ...
+{
+  pkgs,
+  lib,
+  dpi,
+  ...
 }:
 let
   DPI = builtins.toString dpi;
@@ -213,6 +214,7 @@ in
   xdg.configFile."rofi/config.rasi".text =
     builtins.replaceStrings [ "DPI" "WIDTH" "HEIGHT" ] [ DPI rofi_width rofi_height ]
       (builtins.readFile ../../dotfiles/rofi);
+  catppuccin.hyprland.enable = false;
   wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.configType = "hyprlang";
   wayland.windowManager.hyprland.settings = {
@@ -366,21 +368,20 @@ in
       # workspaces
       # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
       builtins.concatLists (
-        builtins.genList
-          (
-            x:
-            let
-              ws =
-                let
-                  c = (x + 1) / 10;
-                in
-                builtins.toString (x + 1 - (c * 10));
-            in
-            [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          ) 10
+        builtins.genList (
+          x:
+          let
+            ws =
+              let
+                c = (x + 1) / 10;
+              in
+              builtins.toString (x + 1 - (c * 10));
+          in
+          [
+            "$mod, ${ws}, workspace, ${toString (x + 1)}"
+            "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+          ]
+        ) 10
       )
     );
   };
