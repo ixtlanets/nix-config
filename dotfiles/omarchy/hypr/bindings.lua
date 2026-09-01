d88351e319581970a1f35ab4ff9806f0253994c2
Sergey Nikulin <snikulin@gmail.com>
2026-09-01 03:15:09 +0300

    [omarchy] rebind screenshot shortcut

--- dotfiles/omarchy/hypr/bindings.lua
+++ dotfiles/omarchy/hypr/bindings.lua
@@ -7,6 +7,10 @@ o.bind("SUPER + D", "Apps", "omarchy-menu toggle apps")
 hl.unbind("SUPER + ALT + D")
 o.bind("SUPER + ALT + D", "Omarchy menu", "omarchy-menu toggle root")
 
+hl.unbind("PRINT")
+hl.unbind("SUPER + SHIFT + S")
+o.bind("SUPER + SHIFT + S", "Screenshot", "omarchy-capture-screenshot")
+
 o.bind("ALT + SPACE", "Toggle keyboard backlight", "kbd-backlight toggle", { locked = true })
 
 o.bind("SUPER + SHIFT + J", "Next window in group", hl.dsp.group.next())
